9d3fd45c4ff3588cc9d0b84a2c864207036de7d4
sahilvish01 <sahilvish6542@gmail.com>
2026-03-24 01:58:21 +0530

    Datatype in JavaScript

---
+++ code95/script.js
@@ -0,0 +1,34 @@
+// Datatype
+
+// Premitive datatype
+let a = null;
+let b = "Hello";
+let c = true;
+let d = 100;
+let e;
+let f = 2.9;
+let g = 1000000;
+
+console.log(a + " " + typeof(a));
+console.log(b + " " + typeof(b));
+console.log(c + " " + typeof(c));
+console.log(d + " " + typeof(d));
+console.log(e + " " + typeof(e));
+console.log(f + " " + typeof(f));
+console.log(g + " " + typeof(g));
+
+// Object
+
+let obj = {
+
+    name : "Sahil",
+    marks : 100
+}
+
+console.log(obj);
+
+// Add more in object
+
+obj.salary = 100000000000000;
+
+console.log(obj);
