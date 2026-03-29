8e1c69fc4da4a28fada0a26b54f8b161d87a46bf
sahilvish01 <sahilvish6542@gmail.com>
2026-03-30 01:54:43 +0530

    Conditional Statement

---
+++ code98/script.js
@@ -0,0 +1,54 @@
+// Conditional Statement
+
+if(true)
+{
+    console.log("if");
+}
+
+if(false)
+{
+    console.log("if");
+}
+else
+{
+    console.log("else");
+}
+
+if(false)
+{
+    console.log("if");
+}
+else if(true)
+{
+    console.log("else if");
+}
+else
+{
+    console.log("else");
+}
+
+// Ternary Operator
+
+let a =100;
+
+(a==100)?console.log("ha 100 hai") : console.log("nahi 100 nahi hai");
+
+
+// Switch Case :
+
+let c = 3;
+
+switch(c)
+{
+    case 1 : console.log(1 + "hai");
+             break;
+    case 2 : console.log(2 + "hai");
+             break;
+    case 3 : console.log(3 + "hai");
+             break;
+    case 4 : console.log(4 + "hai");
+             break;
+    case 5 : console.log(5 + "hai");
+             break;
+    default : console.log("Kuch nahi hai");
+}
