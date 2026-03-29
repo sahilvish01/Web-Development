72ad4ce328826c1721ba53daf2ecd8a0dd21d9a3
sahilvish01 <sahilvish6542@gmail.com>
2026-03-30 01:54:18 +0530

    Operators

---
+++ code97/script.js
@@ -0,0 +1,21 @@
+// Operators
+
+let a = 10;
+let b = 2;
+
+console.log(a**b);      //Exponentiation operator
+
+a **= b;
+
+console.log(a);
+
+// Comparision Operators
+
+console.log("3" == 3);      //Equal to
+
+console.log("3" === 3);     //Equal value and type
+
+console.log(a != b);        //Not equal to
+
+console.log(a !== b);       // Equal to or type of
+
