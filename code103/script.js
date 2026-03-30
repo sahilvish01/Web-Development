cafd35ef9dd0b9de32cc286cbe321435c8f498cf
sahilvish01 <sahilvish6542@gmail.com>
2026-03-31 01:04:54 +0530

    Javascript Rivision

---
+++ code103/script.js
@@ -0,0 +1,26 @@
+// Rivision 
+
+
+console.log("Rivision");
+
+// Variable :
+
+{
+    var a = 100;    // Globally Scoped
+}
+
+
+console.log(a);
+
+{
+    let b = 100;    // Locally scoped
+}
+
+// console.log(b);      // Not Acccessable 
+
+
+const c = 100;      //Constant
+
+// c = 200;    // Not allowed
+
+
